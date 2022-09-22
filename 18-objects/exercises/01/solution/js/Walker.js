4aebdebd9b71df469e81de810aa48b831e71dcd5
tamaslud <tamaslud@yahoo.com>
2022-09-22 19:33:46 +0200

    funkciók

--- 18-objects/exercises/01/solution/js/Walker.js
+++ 18-objects/exercises/01/solution/js/Walker.js
@@ -1,1 +1,35 @@
+function Walker(name = 'John Doe', state = 'dead') {
+  this.name = name;
+  this.state = state;
+}
+Walker.prototype = {
+  walk(speedInMph = 10) {
+    return `${this.name} the ${this.state} walks with ${speedInMph} mph!`;
+  },
+  eat(foodName = 'meat') {
+    return `${this.name} the ${this.state} eats ${foodName}!`;
+  },
+  speak(sound = 'grrrrrr') {
+    return `${this.name} the ${this.state} says ${sound} !`;
+  },
+};
+
+
+
+
 export default Walker;
+/*
+const Walker = {
+    name: 'John Doe',
+    state: 'dead',
+    walk: function(speedInMph) {
+        return this.name + ' the ' + this.state + ' walks with ' + speedInMph + ' mph!'
+    },
+    eat: function(foodName) {
+        return this.name + ' the ' + this.state + ' eats ' + foodName + '!'
+    },
+    speak: function(sound){
+        return this.name + ' the ' + this.state + ' says ' + sound + '!'
+    }
+}
+*/

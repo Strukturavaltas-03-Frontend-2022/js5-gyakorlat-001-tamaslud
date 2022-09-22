eb37d3855963d792430b0f9ec7564ca7f42a05e2
tamaslud <tamaslud@yahoo.com>
2022-09-22 19:52:30 +0200

    getters setters

--- 18-objects/exercises/01/solution/js/Walker.js
+++ 18-objects/exercises/01/solution/js/Walker.js
@@ -10,14 +10,25 @@ Walker.prototype = {
     return `${this.name} the ${this.state} eats ${foodName}!`;
   },
   speak(sound = 'grrrrrr') {
-    return `${this.name} the ${this.state} says ${sound} !`;
+    return `${this.name} the ${this.state} says ${sound}!`;
   },
+  get firstName() {
+    return this.name.split(' ')[0];
+  },
+  set firstName(value) {
+    this.name = `${value} ${this.name.split(' ')[1]}`;
+  },
+  get lastName() {
+    return this.name.split(' ')[1];
+  },
+  set lastName(value) {
+    this.name = `${this.name.split(' ')[0]} ${value}`;
+  },
+
 };
 
-
-
-
 export default Walker;
+
 /*
 const Walker = {
     name: 'John Doe',
